b492b1adb15e78abf14a7590661ca574c9782919
claurojass23 <128934493+claurojass23@users.noreply.github.com>
2023-08-10 12:55:20 -0500

    frase&imagen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,12 @@ import img from '../imagenes/arepashome.jpg'
 export default function Home() {
   return (
     <>
+    <div className='contenedorFrase'>
     <img className='HomeImg' src={img}></img>
+    <h2 className='h2'>Nuestro sazón venezolano, una experiencia para tu paladar.</h2>
+    </div>
+    
+    
     </>
   )
 }
